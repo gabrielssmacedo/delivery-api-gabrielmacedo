d62c6bfa5621a5eb82e2b3b099c9f7ecbfa0ad63
gabrielssmacedo <gabrielmacedo317@gmail.com>
2025-07-08 14:36:32 +0000

    feat: modeling entities (Cliente, ItemPedido, Pedido, Produto, Restaurante, StatusPedido)

--- src/main/java/com/deliverytech/delivery/controller/HealthController.java
+++ src/main/java/com/deliverytech/delivery/controller/HealthController.java
@@ -1,4 +1,4 @@
-package com.deliverytech.delivery;
+package main.java.com.deliverytech.delivery.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
