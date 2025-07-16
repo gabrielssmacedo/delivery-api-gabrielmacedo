11dafa5d8d2d8690bd8b9c66cad12139ad302ac9
gabrielssmacedo <gabrielmacedo317@gmail.com>
2025-07-15 22:04:44 -0300

    feat: create ClienteController

--- src/main/java/com/deliverytech/delivery/model/Cliente.java
+++ src/main/java/com/deliverytech/delivery/model/Cliente.java
@@ -1,26 +1,41 @@
 package com.deliverytech.delivery.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
-@Entity
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Cliente {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
+
+    @Column(unique = true)
     private String email;
     private String telefone;
     private String endereco;
-    private boolean ativo;
+
+    @Builder.Default
+    private boolean ativo = true;
+
+    @Builder.Default
+    private LocalDateTime dataCriacao = LocalDateTime.now();
 
     @OneToMany(mappedBy = "cliente")
-    private List<Pedido> pedidos;
+    @Builder.Default
+    private List<Pedido> pedidos = new ArrayList<>();
 
     public void inativar() {
-       
         throw new UnsupportedOperationException("Unimplemented method 'inativar'");
     }
 }
